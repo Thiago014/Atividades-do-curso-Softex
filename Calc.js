acd6036489562b01de9878c3c286eef6c0885d34
Thiago Rocha <106111905+Thiago014@users.noreply.github.com>
2022-10-19 11:08:53 -0300

    Create Calc.js

---
+++ Calc.js
@@ -0,0 +1,25 @@
+var n1 = prompt ('digite o primeiro numero');
+var n2 = prompt ('digite o segundo numero');
+op = prompt ('digite qual vai ser a operacao');
+
+switch(op) { 
+  
+ case "+" 
+ alert:("Resultado:" n1+n2) 
+ break 
+  
+ case "-" 
+ alert:("Resultado:" n1-n2) 
+ break 
+  
+ case ("*") 
+ alert:("Resultado:" n1*n2) 
+ break 
+  
+ case ("/") 
+ alert:("Resultado:" n1/n2) 
+ break 
+      
+ default:alert("operacao inválida")  
+  
+ }
