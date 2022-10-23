0b9efb664c2ddaa7498f70f0c1eb04cc5ead774f
Thiago Rocha <106111905+Thiago014@users.noreply.github.com>
2022-10-23 11:10:43 -0300

    Create Notas.js

---
+++ Notas.js
@@ -0,0 +1,16 @@
+nota1 = prompt ('digite a primeira nota: ');
+nota2 = prompt ('digite a segunda nota: ');
+nota3 = prompt ('digite a terceira nota: ');
+
+notafinal = (nota1 + nota2 + nota3)/3
+resultado = (notafinal>=7 ? 'aprovado' : 'reprovado')
+
+prompt = resultado 
+
+_________________________________
+
+nota1 = prompt ('Digite a sua primeira nota = '); 
+nota2 = prompt ('Digite a sua segunda nota = ') 
+      
+nota3 = 21 - (nota1+nota2)   
+Alerta ('A nota mínima necessária para ser aprovado é : ' + nota3)
